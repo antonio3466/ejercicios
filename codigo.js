e7be6211b67b18c0f7b413daf5c5d08ead885f29
antonio vera <jorgevera8459@gmail.com>
2025-05-20 20:51:50 -0300

    ejercicio

--- codigo.js
+++ codigo.js
@@ -1,6 +1,40 @@
- var v1 = document .getElementById("variable");
-  var nombre = "edwin"
-  var apellido = "machuca chavez"
-  var resultado1 = nombre + ' ' + apellido 
+ var v1 = document .getElementById("var_texto");
+ var v2 = document .getElementById("var_Entera");
+ var v3 = document .getElementById ("var_Booleana");
 
- v1.innerText +=  ' ' + resultado1  ;
+
+// tipo string (texto)
+  var nombre = "edwin";
+// tipo number (numerico)
+  var edad = 50.2
+
+// tipo boolean (booleano)
+  var vive = true;
+
+
+// tipo array (arreglo - vector)  
+
+var vector = {'juan','padre' ,'mauel',true , 13.37 }
+
+
+
+
+
+//Operaciones
+
+
+var opNombre = 'colegio' + 'ET Nro 12'
+var opEdad = 12.2 + 12.3
+var opVive = false + false;
+
+
+
+
+//muetro las variables en los respentivos elementos  
+ v1.innerText +=  ' ' + opNombre  ;
+ v2.innerText +=  ' ' + Edad     ;
+ v3.innerText +=  ' ' + vive  ;
+
+ //v3.innerText +=  ' ' + vector ; 
+
+for (var 1=0; i<=4 ; i++)
